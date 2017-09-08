b93b0fdb9e4b7227e4656d3694dfc94b615efe92
Daniel Kambich <dkambich@purdue.edu>
2017-09-07 22:10:29 -0400

    Formatting Prim Algorithm

--- Maze Generation Source Code/Prim.java
+++ Maze Generation Source Code/Prim.java
@@ -1,64 +1,71 @@
-import java.util.ArrayList;
 /**
- * Prim.java  
+ * Prim.java
  *
  * @author: Daniel Kambich
- * 
+ *
  */
-public class Prim extends MazeGenerator
-{
-    private ArrayList<Wall> walls;
-    private Cell current;
-    private Wall nextWall;
-    public Prim(Maze maze){
-        this.maze=maze;
+
+import java.util.ArrayList;
+
+public class Prim extends MazeGenerator {
+  private ArrayList<Wall> walls;
+  private Cell current;
+  private Wall nextWall;
+
+  public Prim(Maze maze) {
+    this.maze=maze;
+  }
+
+  public void startMaze(int width, int height, int numCells) {
+    initializeMaze(width, height, numCells);
+    walls = new ArrayList<Wall>();
+    current = maze.cells[(int) (Math.random() * numCells)][(int) (Math.random() * numCells)];
+    walls.addAll(generateWalls(current, getUnvisitedNeighbors(current.getRow(), current.getCol())));
+  }
+
+  public void stepMaze() {
+    if(walls.size() > 0) {
+      if(nextWall != null) {
+        nextWall.toCell.isCurrent = false;
+      }
+      int nextIndex = (int) (Math.random() * walls.size());
+      nextWall = walls.get(nextIndex);
+      current = nextWall.fromCell;
+      if(!nextWall.toCell.visited) {
+        nextWall.createPassage();
+        nextWall.toCell.isCurrent = true;
+        walls.addAll(generateWalls(nextWall.toCell, getUnvisitedNeighbors(nextWall.toCell.getRow(), nextWall.toCell.getCol())));
+      }
+      walls.remove(nextWall);
+    }
+    else {
+      current.isCurrent = false;
+    }
+  }
+
+  public ArrayList<Wall> generateWalls(Cell fromCell, ArrayList<Cell> toCells){
+    ArrayList<Wall> walls = new ArrayList<Wall>();
+    for(Cell cell: toCells){
+      Wall next = new Wall(fromCell, cell);
+      if(!next.toCell.visited) {
+        walls.add(next);
+      }
+    }
+    return walls;
+  }
+
+  public class Wall {
+    protected Cell fromCell, toCell;
+
+    public Wall(Cell from, Cell to) {
+      fromCell = from;
+      toCell = to;
     }
 
-    public void startMaze(int width, int height, int numCells){
-        initializeMaze(width, height, numCells);
-        walls=new ArrayList<Wall>();
-        current=maze.cells[(int)(Math.random()*numCells)][(int)(Math.random()*numCells)];
-        walls.addAll(generateWalls(current, getUnvisitedNeighbors(current.getRow(), current.getCol())));     
+    public void createPassage(){
+      removeWall(fromCell, toCell);
+      fromCell.visited = true;
+      toCell.visited = true;
     }
-
-    public void stepMaze(){
-        if(walls.size()>0){
-            if(nextWall != null)
-                nextWall.toCell.isCurrent=false;
-            int nextIndex=(int)(Math.random()*walls.size());
-            nextWall=walls.get(nextIndex);
-            current=nextWall.fromCell;
-            if(!nextWall.toCell.visited){   
-                nextWall.createPassage();         
-                nextWall.toCell.isCurrent=true;
-                walls.addAll(generateWalls(nextWall.toCell, getUnvisitedNeighbors(nextWall.toCell.getRow(), nextWall.toCell.getCol())));           
-            }
-            walls.remove(nextWall);            
-        }
-        else
-            current.isCurrent=false;
-    }
-
-    public ArrayList<Wall> generateWalls(Cell fromCell, ArrayList<Cell> toCells){
-        ArrayList<Wall> walls=new ArrayList<Wall>();
-        for(Cell cell: toCells){
-            Wall next=new Wall(fromCell, cell);
-            if(!next.toCell.visited)
-                walls.add(next);
-        }
-        return walls;
-    }
-
-    public class Wall{
-        protected Cell fromCell, toCell;
-        public Wall(Cell from, Cell to){
-            fromCell=from;
-            toCell=to;
-        }
-
-        public void createPassage(){
-            removeWall(fromCell, toCell);
-            fromCell.visited=true; toCell.visited=true;       
-        }
-    }
+  }
 }
